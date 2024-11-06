5067e923790da79ef19b4c41630b5d05236aaa68
thirumurugan7 <thiru82003@gmail.com>
2024-11-06 22:59:14 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,44 @@
+'use client'
+ 
+import { useRouter } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
+import { Helmet } from 'react-helmet';
+
+import Image from "next/image";
+
+export default function Home() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+
+  const search = searchParams.get('r')
+
+  const playerUrl = search ? `https://checkapp.vercel.app/r=${r}` : '';
+
+
+  return (
+    <>
+    <Helmet>
+      <meta name="twitter:card" content="player" />
+      <meta name="twitter:title" content="Minting Wink" />
+      <meta name="twitter:description" content="Minting NFT on X" />
+      {/* Dynamically update twitter:player */}
+      <meta name="twitter:player" content={playerUrl} />
+      <meta name="twitter:player:width" content="360" />
+      <meta name="twitter:player:height" content="560" />
+      <meta name="twitter:image" content="https://res.cloudinary.com/dg8ssfxu3/image/upload/v1722584912/ud/y6uypvxozxsutmrkhot0.png" />
+      <meta name="twitter:site" content="https://x.com/ultimatedigits" />
+      <meta name="twitter:app:card" content="player" />
+      <title>Ultimate Winks</title>
+    </Helmet>
+
+    <div>
+      <h1>Welcome to Ultimate Winks!</h1>
+      <p>Mint your NFT by passing the "r" query parameter to the URL.</p>
+      <p>
+        Example: <code>{playerUrl}</code>
+      </p>
+    </div>
+  </>
+  );
+}
